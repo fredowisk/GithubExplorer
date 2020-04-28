2f05fd25f59d871d72c6defdcab5df8560972e98
Fredowisk <paulorobertoparreira@yahoo.com.br>
2020-04-28 17:24:09 -0300

    Updating the code

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 import { Title, Form, Repositories, Error } from './styles';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const logoImg = require('../../assets/logo.svg');
+import logoImg from '../../assets/logo.svg';
 
 interface Repository {
   full_name: string;
@@ -80,7 +80,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -91,7 +94,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
